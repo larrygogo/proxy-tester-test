d4bde9fbb3c95a9bc17d0dca7a63edc433b9672d
larrygogo <351220018@qq.com>
2023-08-29 11:23:16 +0800

    test

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -6,14 +6,17 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
 
     // 仅在 macOS 下执行
     #[cfg(target_os = "macos")]
-    window_vibrancy::apply_vibrancy(&window, window_vibrancy::NSVisualEffectMaterial::HudWindow, None, None)
-        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+    window_vibrancy::apply_vibrancy(
+        &window,
+        window_vibrancy::NSVisualEffectMaterial::HudWindow,
+        Option::from(window_vibrancy::NSVisualEffectState::Active),
+        None
+    ).expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
     // 设置 titleBarStyle
 
     // 仅在 windows 下执行
     #[cfg(target_os = "windows")]
     window_vibrancy::apply_blur(&window, Some((18, 18, 18, 125)))
         .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-
     Ok(())
 }
